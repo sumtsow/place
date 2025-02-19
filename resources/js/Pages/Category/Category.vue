ba8e12fd05eccd964b242a3405ffdbc0bd31fbbe
Sumtsow <sumtsow@gmail.com>
2025-02-19 12:55:38 +0200

    Homepage code separated into templates.

---
+++ resources/js/Pages/Category/Category.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+	category: {
+        type: Object,
+	},
+});
+</script>
+
+<template>
+	<div class="pt-3 sm:pt-5">
+		<h2 class="text-xl font-semibold text-black dark:text-white" >
+			{{ category.name }}
+		</h2>
+		<p v-for="subcategory in category.subcategories" class="mt-4 text-sm/relaxed">
+			{{ subcategory.name }}
+		</p>
+	</div>
+</template>
