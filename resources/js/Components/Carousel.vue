93e3566f42ac6c58ea323405f0125a10506c5359
Sumtsow <sumtsow@gmail.com>
2025-08-25 12:56:56 +0300

    Items images slider on item view page added.

---
+++ resources/js/Components/Carousel.vue
@@ -0,0 +1,29 @@
+<script setup>
+
+defineProps({
+	images: {
+        type: Array,
+	},
+});
+</script>
+
+<template>
+	<div id="imageCarousel" class="carousel slide carousel-dark">
+		<div class="carousel-indicators mb-0">
+			<button v-for="image, key in images" type="button" data-bs-target="#imageCarousel" :data-bs-slide-to="key" :class="{ active: key == 0 }" :aria-current="!key" :aria-label="'Slide ' + ++key"></button>
+		</div>
+		<div class="carousel-inner">
+			<div v-for="image, key in images" class="carousel-item" :class="{ active: key == 0 }">
+				<img :src="'/storage/img/' + image" :alt="'image-' + key" class="d-block" style="height: 256px; width: auto; margin: 30px auto;">
+			</div>
+		</div>
+		<button class="carousel-control-prev" type="button" data-bs-target="#imageCarousel" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#imageCarousel" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+	</div>
+</template>
--- resources/js/Components/Item.vue
+++ resources/js/Components/Item.vue
@@ -1,4 +1,5 @@
 <script setup>
+import Carousel from '@/Components/Carousel.vue';
 import { Link } from '@inertiajs/vue3';
 
 defineProps({
@@ -12,13 +13,15 @@ defineProps({
 	<div class="row">
 		<div class="col">
 			<div class="card h-100 justify-content-center">
-				<img :src="JSON.parse(item.images) ? '/storage/img/' + JSON.parse(item.images)[0] : '/storage/img/no-image.png'" class="card-img-top" :alt="'image-' + item.id" style="height: 256px; width: auto; margin: 30px auto;">
+				<template v-if="JSON.parse(item.images)">
+					<carousel v-if="JSON.parse(item.images).length > 1" :images="JSON.parse(item.images)"></carousel>
+					<img v-else :src="'/storage/img/' + JSON.parse(item.images)[0]" class="card-img-top" :alt="'image-' + item.id" style="height: 256px; width: auto; margin: 30px auto;">
+				</template>
+				<img v-else src="/storage/img/no-image.png" class="card-img-top" :alt="'image-' + item.id" style="height: 256px; width: auto; margin: 30px auto;">
 				<div class="card-body">
 					<h5 class="card-title">{{ item.name }}</h5>
 					<p class="card-text">
-						<Link :href="route('item', [item.id])" class="stretched-link">
-							{{ item.description }}
-						</Link>
+						{{ item.description }}
 					</p>
 				</div>
 			</div>
